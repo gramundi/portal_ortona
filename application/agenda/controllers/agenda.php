96518edbecb9edca103336f8365a67995f622a84
Giovanni.Ramundi <Giovanni.Ramundi@parexelcloud.com>
2020-05-09 14:03:07 +0100

    revert

--- application/agenda/controllers/agenda.php
+++ application/agenda/controllers/agenda.php
@@ -2,7 +2,7 @@
 
 class agenda extends CI_Controller {
 
-        // var per variabili della classe;
+        var per variabili della classe;
         var $offset=0;
         //var $limit=0;
         var $run=0;
